5783769f54aa39844a62da8734cfcdaa0d272dcf
DongHyun <hyde981114@gmail.com>
2023-01-10 15:52:46 +0900

    feat: BFS & DFS(preorder, postorder, inorder)

--- tree/binaryTree.ts
+++ tree/binaryTree.ts
@@ -71,4 +71,82 @@ class BinaryTree {
 
     return undefined;
   }
+
+  // O(n)
+  bfs() {
+    const result: TreeNode[] = [];
+
+    const queue: TreeNode[] = [];
+
+    let currentNode = this.root;
+
+    if (currentNode !== null) queue.unshift(currentNode);
+
+    while (queue.length !== 0) {
+      currentNode = queue.shift() as TreeNode;
+
+      if (currentNode.left !== null) queue.push(currentNode.left);
+
+      if (currentNode.right !== null) queue.push(currentNode.right);
+    }
+
+    return result;
+  }
+
+  // O(n)
+  dfsPreorder() {
+    const result: TreeNode[] = [];
+
+    let currentNode = this.root;
+
+    function traverse(node: TreeNode) {
+      result.push(node);
+
+      if (node.left !== null) traverse(node.left);
+
+      if (node.right !== null) traverse(node.right);
+    }
+
+    if (currentNode !== null) traverse(currentNode);
+
+    return result;
+  }
+
+  // O(n)
+  dfsPostorder() {
+    const result: TreeNode[] = [];
+
+    let currentNode = this.root;
+
+    function traverse(node: TreeNode) {
+      if (node.left !== null) traverse(node.left);
+
+      if (node.right !== null) traverse(node.right);
+
+      result.push(node);
+    }
+
+    if (currentNode !== null) traverse(currentNode);
+
+    return result;
+  }
+
+  // O(n)
+  dfsInorder() {
+    const result: TreeNode[] = [];
+
+    let currentNode = this.root;
+
+    function traverse(node) {
+      if (node.left !== null) traverse(node.left);
+
+      result.push(node);
+
+      if (node.right !== null) traverse(node.right);
+    }
+
+    if (currentNode !== null) traverse(currentNode);
+
+    return result;
+  }
 }
