f167c61d01b547a260cd3defde5b0dc941772aa2
DongHyun <hyde981114@gmail.com>
2023-01-10 19:29:55 +0900

    feat: Max Binary Heap

---
+++ tree/binaryHeap/maxBinaryHeap.ts
@@ -0,0 +1,81 @@
+// Max Binary Heap
+
+class MaxBinaryHeap {
+  values: number[];
+
+  constructor() {
+    this.values = [];
+  }
+
+  // O(logn)
+  insert(value: number) {
+    this.values.push(value);
+
+    this.bubbleUp();
+
+    return this.values;
+  }
+
+  bubbleUp() {
+    let currentIndex = this.values.length - 1;
+    const insertedValue = this.values[currentIndex];
+
+    while (currentIndex > 0) {
+      let parentIndex = Math.floor((currentIndex - 1) / 2);
+      let parentValue = this.values[parentIndex];
+
+      if (insertedValue > parentValue) {
+        this.values[parentIndex] = insertedValue;
+        this.values[currentIndex] = parentValue;
+        currentIndex = parentIndex;
+      } else break;
+    }
+  }
+
+  // O(logn)
+  extractMax() {
+    const maxValue = this.values[0];
+    const endValue = this.values.pop();
+
+    if (this.values.length > 0) {
+      this.values[0] = endValue as number;
+
+      this.sinkDown();
+    }
+
+    return maxValue;
+  }
+
+  sinkDown() {
+    let currentIndex = 0;
+    let currentValue = this.values[currentIndex];
+
+    while (true) {
+      let leftChildIndex = 2 * currentIndex + 1;
+      let rightChildIndex = 2 * currentIndex + 2;
+
+      let leftChildValue = this.values[leftChildIndex];
+      let rightChildValue = this.values[rightChildIndex];
+
+      let swapIndex: number | null = null;
+
+      if (leftChildIndex < this.values.length && leftChildValue > currentValue)
+        swapIndex = leftChildIndex;
+
+      if (rightChildIndex < this.values.length) {
+        if (
+          (swapIndex === null && rightChildValue > currentValue) ||
+          (swapIndex !== null && rightChildValue > leftChildValue)
+        )
+          swapIndex = rightChildIndex;
+      }
+
+      if (swapIndex === null) break;
+
+      this.values[currentIndex] = this.values[swapIndex];
+      this.values[swapIndex] = currentValue;
+
+      currentIndex = swapIndex;
+    }
+  }
+}
